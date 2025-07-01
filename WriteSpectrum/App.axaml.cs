405e42e001f1235b695a891652f555fbd07c41a0
simkaa47 <simkaa7@gmail.com>
2025-07-01 16:40:51 +0400

    Добавлено окно WriteSpectrum и начальная реализация приложения

---
+++ WriteSpectrum/App.axaml.cs
@@ -0,0 +1,25 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using WriteSpectrum.ViewModels;
+
+namespace WriteSpectrum;
+
+public partial class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.MainWindow = new MainWindow();
+            desktop.MainWindow.DataContext = new MainViewModel();
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
---
+++ WriteSpectrum/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace WriteSpectrum;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
